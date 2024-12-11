20aaaf9a8304b7c7a01a138d97e32d5d89c68878
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-11 19:35:45 -0300

    init y config

---
+++ src/main/java/com/pawpengaga/DrillingJpaClasesApplication.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrillingJpaClasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrillingJpaClasesApplication.class, args);
+	}
+
+}
